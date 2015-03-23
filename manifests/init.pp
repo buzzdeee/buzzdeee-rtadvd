40d36fd79143394bc42e539447a148dd56b931aa
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2015-03-23 16:17:19 +0100

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,49 @@
+# == Class: rtadvd
+#
+# Full description of class rtadvd here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { 'rtadvd':
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2014 Your name here, unless otherwise noted.
+#
+class rtadvd (
+  $service_ensure  = 'running',
+  $service_enable  = true,
+  $service_flags   = undef,
+){
+
+  service { "$name":
+    ensure  => $service_ensure,
+    enable  => $service_enable,
+    flags   => $service_flags,
+  }
+}
